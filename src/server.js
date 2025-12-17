583e60170c1efd951db0af170bcaf56de1b81632
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2025-12-17 11:22:02 -0300

    Organizando a estrutura inicial do projeto e servidor Express

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+// Middleware para ler JSON
+app.use(express.json())
+
+// Middleware de autenticação por token fixo
+app.use((req, res, next) => {
+  const auth = req.headers.authorization
+
+  if (auth !== 'Bearer MEU_TOKEN_FIXO') {
+    return res.status(401).json({ error: 'Não autorizado' })
+  }
+  //
+  next()
+})
+
+// Servidor
+app.listen(3000, () => {
+  console.log('API rodando na porta 3000')
+})
