98c8de3dce3ec2de73fccce6d5b8395e657da944
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2026-01-12 18:50:42 -0300

    refactor: migrate cities and states API from in-memory to MySQL

--- src/controllers/cidadesController.js
+++ src/controllers/cidadesController.js
@@ -1,103 +1,156 @@
-const { estados } = require('./estadosController')
+// const { estados } = require('./estadosController')
 
-let cidades = [
-  { id: 1, nome: 'Campinas', estado_uf: 'SP' },
-  { id: 2, nome: 'Rio de Janeiro', estado_uf: 'RJ' },
-  { id: 3, nome: 'Belo Horizonte', estado_uf: 'MG' },
-  { id: 4, nome: 'Curitiba', estado_uf: 'PR' },
-  { id: 5, nome: 'Porto Alegre', estado_uf: 'RS' },
-]
+const db = require('../database/connection')
 
-function listarCidades(req, res) {
-  res.json(cidades)
+async function listarCidades(req, res) {
+  try {
+    const [rows] = await db.query('SELECT * FROM cidades')
+    res.json(rows)
+  } catch {
+    res.status(500).json({ error: 'Erro ao buscar estados' })
+    console.log(error)
+  }
 }
 
-function listarCidadePorId(req, res) {
+async function listarCidadePorId(req, res) {
   const id = parseInt(req.params.id)
-  const cidade = cidades.find((c) => c.id === id)
-
-  if (!cidade) {
-    return res.status(404).json({ error: 'Cidade não encontrada' })
+  if (isNaN(id)) {
+    return res.status(400).json({ error: 'Id inválido' })
   }
 
-  res.json(cidade)
+  try {
+    const [rows] = await db.query('SELECT * FROM cidades WHERE id = ?', [id])
+    if (rows.length === 0) {
+      return res.status(404).json({ error: 'Cidade não encontrada' })
+    }
+
+    res.json(rows[0])
+  } catch {
+    res.status(500).json({ error: 'Erro ao buscar cidades' })
+    console.log(error)
+  }
 }
 
-function criarCidade(req, res) {
-  const { nome, estado_uf } = req.body
-  // console.log(nome)
-
-  if (!nome || !estado_uf) {
-    return res.status(400).json({ error: 'Nome e estado_uf são obrigatórios' })
-  }
-
-  const cidadeExiste = cidades.some((e) => {
-    return (
-      e.nome.toLowerCase() === nome.toLowerCase() &&
-      e.estado_uf.toLowerCase() === estado_uf.toLowerCase()
-    )
-  })
-
-  console.log(cidadeExiste)
-  if (cidadeExiste) {
-    return res.status(409).json({ error: 'Estado e cidades já cadastrado' })
-  }
-
-  const estadoExiste = estados.some((e) => e.uf === estado_uf)
-
-  if (!estadoExiste) {
-    return res.status(400).json({ error: 'Estado não existe' })
-  }
-
-  const novaCidade = {
-    id: cidades.length + 1,
-    nome,
-    estado_uf,
-  }
-
-  cidades.push(novaCidade)
-  res.status(201).json(novaCidade)
-}
-
-function atualizarCidade(req, res) {
-  const id = parseInt(req.params.id)
+async function criarCidade(req, res) {
   const { nome, estado_uf } = req.body
 
-  const cidade = cidades.find((c) => c.id === id)
+  try {
+    // validar
+    if (nome !== undefined && nome.trim() === '') {
+      return res.status(400).json({ error: 'Nome inválido' })
+    }
+    if (!estado_uf || estado_uf.length !== 2) {
+      return res.status(400).json({ error: 'Unidade Federal inválida' })
+    }
 
-  if (!cidade) {
-    return res.status(404).json({ error: 'Cidade não encontrada' })
-  }
+    if (!nome || !estado_uf) {
+      return res
+        .status(400)
+        .json({ error: 'Nome e estado_uf são obrigatórios' })
+    }
 
-  if (nome !== undefined && nome.trim() === '') {
-    return res.status(400).json({ error: 'Nome inválido' })
-  }
+    const [estados] = await db.query('SELECT * FROM estados WHERE uf = ?', [
+      estado_uf,
+    ])
 
-  if (estado_uf) {
-    const estadoExiste = estados.some((e) => e.uf === estado_uf)
-    if (!estadoExiste) {
+    if (estados.length === 0) {
       return res.status(400).json({ error: 'Estado não existe' })
     }
+
+    const [cidades] = await db.query(
+      'SELECT * FROM cidades WHERE nome = ? AND estado_uf = ?',
+      [nome, estado_uf]
+    )
+
+    if (cidades.length > 0) {
+      return res
+        .status(409)
+        .json({ error: 'Cidade já cadastrada nesse estado' })
+    }
+    //inserir cidade
+    const [result] = await db.query(
+      'INSERT INTO cidades (nome, estado_uf) VALUES (?, ?)',
+      [nome, estado_uf]
+    )
+
+    res.status(201).json({
+      id: result.insertId,
+      nome,
+      estado_uf,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Erro ao criar cidade' })
   }
-
-  if (nome) cidade.nome = nome
-  if (estado_uf) cidade.estado_uf = estado_uf
-
-  res.json(cidade)
 }
 
-function removerCidade(req, res) {
+async function atualizarCidade(req, res) {
   const id = parseInt(req.params.id)
-  const index = cidades.findIndex((c) => c.id === id)
-
-  if (index === -1) {
-    return res.status(404).json({ error: 'Cidade não encontrada' })
+  if (isNaN(id)) {
+    return res.status(400).json({ error: 'Id inválido' })
   }
+  const { nome, estado_uf } = req.body
 
-  cidades.splice(index, 1)
-  res.status(204).send()
+  try {
+    const [cidade] = await db.query('SELECT * FROM cidades WHERE id = ?', [id])
+
+    if (nome !== undefined && nome.trim() === '') {
+      return res.status(400).json({ error: 'Nome inválido' })
+    }
+    if (!estado_uf || estado_uf.length !== 2) {
+      return res.status(400).json({ error: 'Unidade Federal inválida' })
+    }
+    if (cidade.length === 0) {
+      return res.status(404).json({ error: 'Cidade não encontrada' })
+    }
+
+    if (nome !== undefined && nome.trim() === '') {
+      return res.status(400).json({ error: 'Nome inválido' })
+    }
+
+    if (estado_uf) {
+      const [estadoExiste] = await db.query(
+        'SELECT * FROM estados WHERE uf = ?',
+        [estado_uf]
+      )
+
+      if (estadoExiste.length === 0) {
+        return res.status(400).json({ error: 'Estado não existe' })
+      }
+    }
+
+    await db.query('UPDATE cidades SET nome = ?, estado_uf = ? WHERE id = ?', [
+      nome ?? cidade[0].nome,
+      estado_uf ?? cidade[0].estado_uf,
+      id,
+    ])
+
+    res.json({ message: 'Cidade atualizada com sucesso' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Erro ao atualizar cidade' })
+  }
 }
 
+async function removerCidade(req, res) {
+  const id = parseInt(req.params.id)
+
+  try {
+    const [cidadeExiste] = await db.query(
+      'SELECT * FROM cidades WHERE id = ?',
+      [id]
+    )
+    if (cidadeExiste.length === 0) {
+      return res.status(404).json({ error: 'Cidade não encontrada' })
+    }
+    await db.query('DELETE FROM cidades WHERE id = ?', [id])
+
+    return res.status(204).send()
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Erro ao remover cidade' })
+  }
+}
 module.exports = {
   listarCidades,
   listarCidadePorId,
--- src/database/connection.js
+++ src/database/connection.js
@@ -3,8 +3,12 @@ const mysql = require('mysql2/promise')
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'W@rafaMYSQL@12',
-  database: 'estados_cidades',
+  password: 'RAFA#231023rafa',
+  database: 'api_estados_cidades',
+  waitForConnections: true,
+  connectionLimit: 10,
+  allowPublicKeyRetrieval: true,
+  useSSL: false,
 })
 
 module.exports = pool
